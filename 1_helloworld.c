6f78a0f8197e9790fbc86cb558f6bca2206a14d5
Kanishka Srivastava <srivastavadev1711@gmail.com>
2022-10-07 18:10:44 +0530

    Contributed efficiently in hacktoberfest 2022

---
+++ 1_helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>   // include
+int main()          // main function 
+{
+    printf("Hello world !");
+    printf("Hola From Zap");
+}
+
+/*
+compile code using:         gcc filename.c
+execute a executable using: ./a.exe (windows) OR ./a.out (linux)
+*/
---
+++ 5_input-output.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("%d is value of a",a);
+    char b[100];
+    printf("\nEnter string: ");
+    scanf("%s",b);
+}
